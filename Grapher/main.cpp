64e73f52735d337caaac40b60291e815c877c717
VaiosChr <vallos1501@gmail.com>
2020-09-01 11:57:05 +0300

    Start of the project

--- Grapher/main.cpp
+++ Grapher/main.cpp
@@ -1,15 +1,10 @@
-//
-//  main.cpp
-//  Grapher
-//
-//  Created by Vaios Christodoulou on 1/9/20.
-//  Copyright © 2020 Vaios Christodoulou. All rights reserved.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+    
+
     return 0;
 }
